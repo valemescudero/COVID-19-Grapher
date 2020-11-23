a3e50edce7afb7e532fb1222729f4332353b0dcb
Valeria Mitchell <57600998+valemescudero@users.noreply.github.com>
2020-11-23 20:48:27 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,3 +1,15 @@
+import requests
+
+def wget(url):
+    r = requests.get(url, allow_redirects=True)
+    with open(url[url.rfind('/') + 1::], 'wb') as f:
+        f.write(r.content)
+
+wget("https://covid.ourworldindata.org/data/ecdc/full_data.csv")
+
+! wget "https://covid.ourworldindata.org/data/ecdc/full_data.csv"
+
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
