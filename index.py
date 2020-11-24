4833418ac46e379841d6f2da1685ad1dcedbfad9
Valeria Mitchell <57600998+valemescudero@users.noreply.github.com>
2020-11-23 22:28:12 -0300

    Update index.py

--- index.py
+++ index.py
@@ -44,54 +44,62 @@ def get_countries():
       print("Opción inválida")
 
 def get_time_frame():
-  dates=['start', 'end']
-  print("1. Modificar rango de tiempo")
-  print("2. Volver al menú principal")
-  next = input("")
-  if next == '1':
-    dates[0] = input("Ingresá fecha de inicio en formato aaaa-mm-dd")
-    dates[1] = input("Ingresá fecha de límite en formato aaaa-mm-dd")
-  elif next == '2':
-      return
-  else:
-    print("Opción inválida")
-  return dates
+  next = '0'
+  dates = ["2020-03-21","2020-11-21"]
+  while next != '2':
+    print("1. Modificar rango de tiempo")
+    print("2. Volver al menú principal")
+    next = input("")
+    if next == '1':
+      dates[0] = input("Ingresá fecha de inicio en formato aaaa-mm-dd")
+      dates[1] = input("Ingresá fecha de límite en formato aaaa-mm-dd")
+    elif next == '2':
+      return dates
+    else:
+      print("Opción inválida")
 
 
 def get_cases(selected_country):
-  cases_dates = list(selected_country["data"]["date"])
-  cases = list(selected_country["data"]["total_cases"])
+  cases_dates = selected_country["data"]["date"]
+  cases = selected_country["data"]["total_cases"]
+  
   plt.plot_date(cases_dates,cases, linestyle="solid", label=selected_country["name"])
+  plt.xticks(rotation=45)
 
 def get_deaths(selected_country):
   deaths_dates = list(selected_country["data"]["date"])
   deaths = list(selected_country["data"]["total_deaths"])
+
   plt.plot_date(deaths_dates,deaths, linestyle="solid", label=selected_country["name"])
+  plt.xticks(rotation=45)
 
 def crear_cruces(first_country, second_country, option):  
+  if (option == '3'):
+    deaths_or_cases = "cases"
+  else:
+    deaths_or_cases = "deaths"
   x_first_country = list(first_country["data"]["date"])
   x_second_country = list(second_country["data"]["date"])
 
-  y_first_country = list(first_country["data"][f"total_{option}"])
-  y_second_country = list(second_country["data"][f"total_{option}"])
-
-  crucex = []
-  crucey = []
+  y_first_country = list(first_country["data"][f"total_{deaths_or_cases}"])
+  y_second_country = list(second_country["data"][f"total_{deaths_or_cases}"])
   
 
   for i in range(1, len(x_first_country)):
+    crucex = []
+    crucey = []
     if (y_first_country[i] == y_second_country[i]) or (y_first_country[i] > y_second_country[i] and y_first_country[i-1] < y_second_country[i-1]) or (y_first_country[i] < y_second_country[i] and y_first_country[i-1] > y_second_country[i-1]):
       crucex.append(x_second_country[i])
       crucey.append(y_second_country[i])
-
-  plt.plot(crucex,crucey, 'ro')
+    plt.plot(crucex,crucey, 'ko')
 
     
 
 def programa():
   option = '0'
   countries = []
-  dates = ['start', 'end']
+  countries_data = []
+  dates = ["2020-03-21","2020-11-21"]
 
   while option != '5':
     print("GRAFICADOR COVID-19\n")
@@ -105,27 +113,30 @@ def programa():
 
     if option == '1':
       countries = get_countries()
-      print("Paises:", countries)
+      print("Países:", countries)
     elif option == '2':
       dates = get_time_frame() 
+    elif option == '3':
       for i in range(len(countries)): 
         countrydata = df[(df["date"].between(dates[0],dates[1]))&(df["location"] == countries[i])]
-        countries[i] = {"name": countries[i], "data": countrydata}
-    elif option == '3':
-      for country in countries:
+        countries_data.append({"name": countries[i], "data": countrydata})
+      for country in countries_data:
         get_cases(country)
-      for i in range(len(countries)-1):
-        for j in range(1,len(countries)):
-          crear_cruces(countries[i], countries[j], "cases")
+      for i in range(len(countries_data)-1):
+        for j in range(1,len(countries_data)):
+          crear_cruces(countries_data[i], countries_data[j], "cases")
       plt.legend()
       plt.show()
       break
     elif option == '4':
-      for country in countries:
+      for i in range(len(countries)): 
+        countrydata = df[(df["date"].between(dates[0],dates[1]))&(df["location"] == countries[i])]
+        countries_data.append({"name": countries[i], "data": countrydata})
+      for country in countries_data:
         get_deaths(country)
-      for i in range(len(countries)-1):
-        for j in range(1,len(countries)):
-          crear_cruces(countries[i], countries[j], "deaths")
+      for i in range(len(countries_data)-1):
+        for j in range(1,len(countries_data)):
+          crear_cruces(countries_data[i], countries_data[j], "deaths")
       
       plt.legend()
       plt.show()
